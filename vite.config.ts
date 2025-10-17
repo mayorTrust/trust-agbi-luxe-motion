3c6e811a389e30ef5e95821a859192324d55bc24
mayorTrust <agbitrust@gmail.com>
2025-10-17 22:18:08 +0100

    Fix Permission

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/trust-agbi-luxe-motion/" : "/",
   server: {
     host: "::",
     port: 8080,
